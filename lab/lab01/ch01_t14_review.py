ac2cfcc283d46f9553ad0d4e820b1deac4d4f367
IPHOWAI-2602 <iphowai1234@gmail.com>
2026-09-04 19:28:32 +0000

    lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -1,5 +1,5 @@
 skill_completed="Python Syntax"
-ercises_completed=13
+exercises_completed=13
 points_per_exercise =5
 point_total=100
 point_total=point_total+exercises_completed*points_per_exercise
