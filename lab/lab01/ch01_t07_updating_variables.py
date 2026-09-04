bda441570f461a907ef16f8216748c4077f4a0f1
IPHOWAI-2602 <iphowai1234@gmail.com>
2026-09-04 17:40:10 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -1,5 +1,4 @@
-September_to_December_ annual_rainfal = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
-annual_rainfall = September_to_December_ annual_rainfal
+annual_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
 
 September_ annual_rainfal = 1.05
 annual_rainfall += September_to_December
