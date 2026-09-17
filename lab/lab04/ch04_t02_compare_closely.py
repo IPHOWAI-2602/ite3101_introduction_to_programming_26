1b9b370d3b47bfec9ca039f5bb4c657d763a3e22
IPHOWAI-2602 <iphowai1234@gmail.com>
2026-09-17 08:15:08 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -7,7 +7,7 @@ bool_one = 17 < 328 True  # We did this one for you!
 bool_two = 100 == (2 * 50) None
 
 # Set this to True if 19 <= 19 or to False if it is not.
-bool_three = None
+bool_three = 19 <= 19 None
 
 # Set this to True if -22 >= -18 or to False if it is not.
 bool_four = None
